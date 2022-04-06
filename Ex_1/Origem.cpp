0b2a2203171d1674bffa94d5d666c6febb55f0f4
SirRoma <35168069+SirRoma@users.noreply.github.com>
2022-04-06 19:58:01 -0300

    Add files via upload

---
+++ Ex_1/Origem.cpp
@@ -0,0 +1,200 @@
+#include <iostream>
+#include <string>
+#include <assert.h>
+#include <../glad/glad.h>
+#include <GLFW/glfw3.h>
+#include "Shader.h"
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <cstdlib>
+#include <Windows.h>
+
+using namespace std;
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
+
+int setupGeometry();
+
+const GLuint WIDTH = 800, HEIGHT = 600;
+
+int main()
+{
+	glfwInit();
+
+	GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "Ex_1  - Otavio", nullptr, nullptr);
+	glfwMakeContextCurrent(window);
+
+	glfwSetKeyCallback(window, key_callback);
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize GLAD" << std::endl;
+
+	}
+
+	// Obtendo as informações de versão
+	const GLubyte* renderer = glGetString(GL_RENDERER); /* get renderer string */
+	const GLubyte* version = glGetString(GL_VERSION); /* version as a string */
+	cout << "Renderer: " << renderer << endl;
+	cout << "OpenGL version supported " << version << endl;
+
+
+	int width = 0, height = 0;
+
+	Shader shader("../shaders/ortho.vs", "../shaders/ortho.fs");
+
+	GLuint VAO = setupGeometry();
+
+	GLint colorLoc = glGetUniformLocation(shader.ID, "inputColor");
+	assert(colorLoc > -1);
+
+	glUseProgram(shader.ID);
+
+	glm::mat4 projection = glm::mat4(1);
+
+	//Alterar aqui para modificar do exercício_1 para o exercício_2
+	//projection = glm::ortho(0.0, 800.0, 600.0, 0.0, -1.0, 1.0);
+	projection = glm::ortho(-10.0, 10.0, -10.0, 10.0, -1.0, 1.0);
+
+	GLint projLoc = glGetUniformLocation(shader.ID, "projection");
+	glUniformMatrix4fv(projLoc, 1, false, glm::value_ptr(projection));
+
+	glm::mat4 model = glm::mat4(1);
+
+	model = glm::translate(model, glm::vec3(0.0, 0, 0));
+	//model = glm::rotate(model, glm::radians(45.0f), glm::vec3(0, 0, 1));
+	//model = glm::scale(model, glm::vec3(2.0, 2.0, 1.0));
+
+	GLint modelLoc = glGetUniformLocation(shader.ID, "model");
+	glUniformMatrix4fv(modelLoc, 1, false, glm::value_ptr(model));
+
+
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwPollEvents();
+		
+
+		glfwGetFramebufferSize(window, &width, &height);
+		glViewport(0, 0, width, height);
+
+		Sleep(1000);
+
+		model = glm::rotate(model, glm::radians(45.0f), glm::vec3(0, 0, 1));
+		GLint modelLoc = glGetUniformLocation(shader.ID, "model");
+		glUniformMatrix4fv(modelLoc, 1, false, glm::value_ptr(model));
+
+		glClearColor(0.8f, 0.8f, 0.8f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+		glLineWidth(5);
+		glPointSize(10);
+		glUniform4f(colorLoc, 0.0f, 1.0f, 1.0f, 1.0f);
+		glDrawArrays(GL_TRIANGLE_STRIP, 0, 3);
+		glUniform4f(colorLoc, 1.0f, 0.0f, 1.0f, 1.0f);
+		glDrawArrays(GL_LINE_LOOP, 0, 3);
+		glBindVertexArray(VAO);
+		glfwSwapBuffers(window);
+
+		Sleep(1000);
+
+		model = glm::rotate(model, glm::radians(90.0f), glm::vec3(0, 0, 1));
+		model = glm::translate(model, glm::vec3(-1.0, 0, 0));
+		modelLoc = glGetUniformLocation(shader.ID, "model");
+		glUniformMatrix4fv(modelLoc, 1, false, glm::value_ptr(model));
+		 
+		glClearColor(0.8f, 0.8f, 0.8f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+		glLineWidth(5);
+		glPointSize(10);
+		glUniform4f(colorLoc, 0.0f, 1.0f, 1.0f, 1.0f);
+		glDrawArrays(GL_TRIANGLE_STRIP, 0, 3);
+		glUniform4f(colorLoc, 1.0f, 0.0f, 1.0f, 1.0f);
+		glDrawArrays(GL_LINE_LOOP, 0, 3);
+		glBindVertexArray(VAO);
+		glfwSwapBuffers(window);
+
+		Sleep(1000);
+
+		model = glm::rotate(model, glm::radians(270.0f), glm::vec3(0, 0, 1));
+		model = glm::translate(model, glm::vec3(1.0, 0, 0));
+		modelLoc = glGetUniformLocation(shader.ID, "model");
+		glUniformMatrix4fv(modelLoc, 1, false, glm::value_ptr(model));
+
+		glClearColor(0.8f, 0.8f, 0.8f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+		glLineWidth(5);
+		glPointSize(10);
+		glUniform4f(colorLoc, 0.0f, 1.0f, 1.0f, 1.0f);
+		glDrawArrays(GL_TRIANGLE_STRIP, 0, 3);
+		glUniform4f(colorLoc, 1.0f, 0.0f, 1.0f, 1.0f);
+		glDrawArrays(GL_LINE_LOOP, 0, 3);
+		glBindVertexArray(VAO);
+		glfwSwapBuffers(window);
+	}
+
+	// Pede pra OpenGL desalocar os buffers
+	glDeleteVertexArrays(1, &VAO);
+	// Finaliza a execução da GLFW, limpando os recursos alocados por ela
+	glfwTerminate();
+	return 0;
+}
+
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode)
+{
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, GL_TRUE);
+}
+
+
+int setupGeometry()
+{
+	// Aqui setamos as coordenadas x, y e z do triângulo e as armazenamos de forma
+	// sequencial, já visando mandar 2para o VBO (Vertex Buffer Objects)
+	// Cada atributo do vértice (coordenada, cores, coordenadas de textura, normal, etc)
+	// Pode ser arazenado em um VBO único ou em VBOs separados
+
+	int m = 3;
+
+	GLfloat vertices[] = {
+		-0.5 * m, -0.5 * m, 0.0,
+		0.5 * m,  -0.5 * m, 0.0,
+		0 * m,  0.5 * m, 0.0,
+		//outro triangulo vai aqui
+	};
+
+	GLuint VBO, VAO;
+
+	//Geração do identificador do VBO
+	glGenBuffers(1, &VBO);
+	//Faz a conexão (vincula) do buffer como um buffer de array
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	//Envia os dados do array de floats para o buffer da OpenGl
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	//Geração do identificador do VAO (Vertex Array Object)
+	glGenVertexArrays(1, &VAO);
+	// Vincula (bind) o VAO primeiro, e em seguida  conecta e seta o(s) buffer(s) de vértices
+	// e os ponteiros para os atributos 
+	glBindVertexArray(VAO);
+	//Para cada atributo do vertice, criamos um "AttribPointer" (ponteiro para o atributo), indicando: 
+	// Localização no shader * (a localização dos atributos devem ser correspondentes no layout especificado no vertex shader)
+	// Numero de valores que o atributo tem (por ex, 3 coordenadas xyz) 
+	// Tipo do dado
+	// Se está normalizado (entre zero e um)
+	// Tamanho em bytes 
+	// Deslocamento a partir do byte zero 
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
+	glEnableVertexAttribArray(0);
+
+	// Observe que isso é permitido, a chamada para glVertexAttribPointer registrou o VBO como o objeto de buffer de vértice 
+	// atualmente vinculado - para que depois possamos desvincular com segurança
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+	// Desvincula o VAO (é uma boa prática desvincular qualquer buffer ou array para evitar bugs medonhos)
+	glBindVertexArray(0);
+
+	return VAO;
+}
+
